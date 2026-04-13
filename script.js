aea142949dfd9dc0be1f1d04b0a1cc82cb176130
Den <127856607+DenBakharev@users.noreply.github.com>
2026-04-13 19:02:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+let score = 0;
+let time = 30;
+
+const duck = document.getElementById("duck");
 const scoreText = document.getElementById("score");
 
+// перемещение утки
 function moveDuck() {
-    duck.style.position = "absolute";
-    duck.style.left = Math.random() * 300 + "px";
-    duck.style.top = Math.random() * 300 + "px";
+  duck.style.position = "absolute";
+  duck.style.left = Math.random() * 300 + "px";
+  duck.style.top = Math.random() * 300 + "px";
 }
 
-duck.onclick = () => {
-    score++;
-    scoreText.innerText = "Score: " + score;
-    moveDuck();
+// клик по утке = +1
+duck.onclick = (e) => {
+  e.stopPropagation(); // чтобы не считался промах
+
+  score++;
+  scoreText.innerText = "Score: " + score;
+
+  duck.style.transform = "scale(0.7)";
+  setTimeout(() => duck.style.transform = "scale(1)", 100);
+
+  moveDuck();
 };
 
-setInterval(moveDuck, 1500);
+// промах = -1
+document.body.onclick = () => {
+  score--;
+  scoreText.innerText = "Score: " + score;
+};
+
+// таймер игры
+setInterval(() => {
+  time--;
+
+  if (time <= 0) {
+    alert("Игра окончена! Score: " + score);
+    location.reload();
+  }
+}, 1000);
+
+// старт
 moveDuck();
